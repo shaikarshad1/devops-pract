b30f9e0f20f84437e4c272c4e95a84f523c00deb
shaikarshad1 <80144453+shaikarshad1@users.noreply.github.com>
2023-10-02 22:21:46 +0530

    first commit

---
+++ src/main/java/com/pro/devopspracct/DevopsPracctApplication.java
@@ -0,0 +1,13 @@
+package com.pro.devopspracct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsPracctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsPracctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/devopspracct/DevopsPracctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.devopspracct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsPracctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
